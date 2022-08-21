b2e846f1e6e7f91a4ec0297a195f54f6b74f77e4
mingikim <mingikim@mingikims-MacBook-Air.local>
2022-08-21 19:57:27 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const express = require('express');
 const app = express();
 const port = 5001;
+const bodyparser = require('body-parser');
+
+const config = require('./config/key');
+
+const { User } = require('./models/User');
+//application/x-www-form-urlencoded
+app.use(bodyparser.urlencoded({ extended: true }));
+//application/json
+app.use(bodyparser.json());
 
 const mongoose = require('mongoose');
 mongoose
-  .connect(
-    'mongodb+srv://kimmingi:mk20471s!@nodeproject.jvqlohw.mongodb.net/?retryWrites=true&w=majority'
-  )
+  .connect(config.mongoURI)
   .then(() => console.log('MongoDB Connected...'))
   .catch((err) => console.log(err));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!민기');
+  res.send('Hello World!!mingi');
+});
+
+app.post('/register', (req, res) => {
+  //회원가입할떄 필요한 정보들을 클라이언트에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다 .
+
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
